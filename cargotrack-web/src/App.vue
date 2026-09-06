81dd3b7e47b39c8ba9896fbe83bf4aa45e24439c
Dikki-FA <dikkifranaalvian@gmail.com>
2026-09-06 18:12:21 +0700

    feat: intial initial setup - Laravel + Vue + PostgreSQL + Tailwind

---
+++ cargotrack-web/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- <HelloWorld /> -->
+   <div class="text-3xl font-bold underline text-blue-600">
+    Test Tailwind
+   </div>
+</template>
